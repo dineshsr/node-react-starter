b640e775117ee777683b751904cdf6b140cbe231
Dinesh Rajendran <srdineshrajendran@gmail.com>
2020-06-06 20:04:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./models/Product');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://dineshsr:Dhinirajsuga@98@passwordmanager-94l45.gcp.mongodb.net/passwordmanager?retryWrites=true&w=majorityr`);
 
 app.use(bodyParser.json());
 
